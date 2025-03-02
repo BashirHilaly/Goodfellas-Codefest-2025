be0f27673351937cdea2fbe79ac7a63f03b99d62
sokoruchi100 <harmeyforever5435@gmail.com>
2025-03-01 19:08:28 -0500

    added global state variable

--- app/(form)/3projDesc.tsx
+++ app/(form)/3projDesc.tsx
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { Link, useRouter } from "expo-router";
+import React, { useContext } from "react";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
 import ProgressBar from "@/components/ui/ProgressBar";
+import DescriptionField from "@/components/ui/DescriptionField";
+import { FormDataContext } from "@/components/ui/FormDataContext";
 
 export default function ProjDescScreen() {
+  const { projectDescription, setProjectDescription } =
+    useContext(FormDataContext);
+
   return (
     <View>
-      <ProgressBar currentStep={2}></ProgressBar>
-      <Text className="text-red-500">
-        Here you input a prompt of what your project is
-      </Text>
+      <ProgressBar currentStep={2} />
+      <Text className="text-6xl">Project Description</Text>
+      <DescriptionField
+        value={projectDescription}
+        onChangeText={(text: string) => setProjectDescription(text)}
+      />
       <Link href="/(form)/4buildMaterials">Continue</Link>
     </View>
   );
